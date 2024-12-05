21b36cc2da2389fc369fd4336fd4bf67233b19ec
Moatiz Sabahat <moatiz.sabahat@hotmail.com>
2024-12-04 21:05:59 -0500

    Initial commit for module 2
    Require assistance

---
+++ UC NetCode2024 Fall/Assets/App/Resources/Scripts/UI/UI_NetManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.UI;
+
+public class UI_NetManager : NetworkBehaviour
+{
+
+    [SerializeField] private Button _serverBttn, _clientBttn, _hostBttn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _hostBttn.onClick.AddListener(HostClick);
+        _clientBttn.onClick.AddListener(ClientClick);
+        _serverBttn.onClick.AddListener(ServerClick);
+    }
+
+
+
+
+    // Update is called once per frame
+    private void ServerClick()
+    {
+        NetworkManager.Singleton.StartServer();
+    }
+    private void ClientClick()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+    private void HostClick()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+}
